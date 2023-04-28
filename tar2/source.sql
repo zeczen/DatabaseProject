25357c5be4f0cf6dad4bc730d14e8864fd9006ba
zeczen <seyal613@gmail.com>
2023-04-28 13:29:55 +0300

    Final queries and their indexes

--- tar2/source.sql
+++ tar2/source.sql
@@ -1,86 +1,109 @@
-SELECT i.S_Company, COUNT(*) AS Num_Ingredients
-FROM Ingredient i
-GROUP BY i.S_Company
-ORDER BY Num_Ingredients DESC;
+-- 1
+SELECT k.K_Name, e.E_FirstName, e.E_LastName, w.E_Salary
+FROM Kitchen k
+         JOIN Work w ON k.K_Name = w.K_Name
+         JOIN Employee e ON w.E_Id = e.E_Id
+WHERE w.E_Salary = (
+    SELECT MAX(w2.E_Salary)
+    FROM Work w2
+    WHERE w2.K_Name = k.K_Name
+)
+  AND k.K_Name IN (
+    SELECT k2.K_Name
+    FROM Kitchen k2
+             JOIN Meal m ON k2.K_Name = m.K_Name
+             JOIN "ORDER" o ON m.M_Id = o.M_Id
+    GROUP BY k2.K_Name
+    HAVING COUNT(DISTINCT o.C_Id) > 1
+);
 
+
+-- 2
+SELECT k.K_Name, SUM(w.E_Salary) AS Total_Salary
+FROM Kitchen k
+         JOIN Work w ON k.K_Name = w.K_Name
+         JOIN Employee e ON w.E_Id = e.E_Id
+WHERE e.E_Age > 67
+GROUP BY k.K_Name;
+
+CREATE INDEX idx_employee_age ON Employee (E_Age);
+
+--3
+SELECT k.K_Name, m.M_Name, MAX(m.M_Price) AS Max_Price
+FROM Kitchen k
+         JOIN Meal m ON k.K_Name = m.K_Name
+         JOIN "ORDER" o ON m.M_Id = o.M_Id
+GROUP BY k.K_Name, m.M_Name
+HAVING COUNT(*) >= ALL (
+    SELECT COUNT(*)
+    FROM "ORDER" o2
+             JOIN Meal m2 ON o2.M_Id = m2.M_Id
+    WHERE m2.K_Name = k.K_Name
+    GROUP BY m2.M_Id
+)
+
+--4
+SELECT e.E_FirstName, e.E_LastName, k.K_Name, COUNT(*) AS Num_Ingredients_In_Stock
+FROM Employee e
+         JOIN Work w ON e.E_Id = w.E_Id
+         JOIN Kitchen k ON w.K_Name = k.K_Name
+         JOIN Stock s ON k.K_Name = s.K_Name
+         JOIN (
+    SELECT K_Name, COUNT(*) AS Num_Ingredients
+    FROM Stock
+    GROUP BY K_Name
+) st ON k.K_Name = st.K_Name
+GROUP BY e.E_FirstName, e.E_LastName, k.K_Name, st.Num_Ingredients
+HAVING COUNT(*) = st.Num_Ingredients;
+
+CREATE INDEX stock_kname_idx ON Stock (K_Name);
+
+--5
 SELECT k.K_Name
 FROM Kitchen k
-WHERE EXISTS (SELECT *
-              FROM Work w
-                       JOIN Employee e ON w.E_Id = e.E_Id
-              WHERE w.K_Name = k.K_Name
-                AND e.E_Age > 50);
+         JOIN Meal m ON k.K_Name = m.K_Name
+         JOIN Recipe r ON m.M_Id = r.M_Id
+         JOIN Ingredient i ON r.I_Id = i.I_Id
+GROUP BY k.K_Name
+HAVING SUM(i.I_Calories) > (SELECT AVG(Total_Calories) FROM
+    (SELECT SUM(i.I_Calories) AS Total_Calories
+     FROM Meal m
+              JOIN Recipe r ON m.M_Id = r.M_Id
+              JOIN Ingredient i ON r.I_Id = i.I_Id
+     GROUP BY m.M_Id) subquery);
 
-SELECT E_FirstName, E_LastName
-FROM Employee
-         JOIN “WORK” ON Employee.E_Id = “WORK”.E_Id
-GROUP BY E_Id, E_FirstName, E_LastName
-HAVING COUNT (DISTINCT K_Name) > 1;
+CREATE INDEX idx_work_salary ON Work (E_Salary);
 
-SELECT M_Name, COUNT(*) AS total_orders
-FROM Order
-         JOIN Meal ON Order.M_Id = Meal.M_Id
-GROUP BY M_Name
-ORDER BY total_orders DESC LIMIT 1;
--- To use indexing, we can create an index on the M_Id column in the Order table since it's being used in the join condition. This index will allow for a faster lookup of rows in the Order table when performing the join operation.
-CREATE INDEX order_m_id ON Order (M_Id);
+--6
+SELECT k.K_Name, e.E_FirstName, e.E_LastName, MAX(m.M_Price) AS Max_Price
+FROM Kitchen k
+         JOIN Meal m ON k.K_Name = m.K_Name
+         JOIN Work w ON k.K_Name = w.K_Name
+         JOIN Employee e ON w.E_Id = e.E_Id
+WHERE m.M_Price = (
+    SELECT MAX(M_Price)
+    FROM Meal
+    WHERE K_Name = k.K_Name
+)
+GROUP BY k.K_Name, e.E_FirstName, e.E_LastName;
 
-SELECT c.C_Id, c.C_FirstName, c.C_LastName, SUM(m.M_Price) AS Total_Cost
-FROM Customer c
-         JOIN "ORDER" o ON c.C_Id = o.C_Id
-         JOIN Meal m ON o.M_Id = m.M_Id
-GROUP BY c.C_Id, c.C_FirstName, c.C_LastName
-ORDER BY Total_Cost DESC;
+CREATE INDEX meal_kname_price_idx ON Meal (K_Name, M_Price);
 
-SELECT m.M_Name, m.M_Price, k.K_Name
-FROM Meal m
-         JOIN Kitchen k ON m.K_Name = k.K_Name
-ORDER BY m.M_Price DESC
+--7
+SELECT k.K_Name, m.M_Name
+FROM Kitchen k
+         JOIN Meal m ON k.K_Name = m.K_Name
+         JOIN Recipe r ON m.M_Id = r.M_Id
+         JOIN Ingredient i ON r.I_Id = i.I_Id
+         JOIN Stock s ON k.K_Name = s.K_Name
+GROUP BY k.K_Name, m.M_Name
+HAVING COUNT(DISTINCT i.I_Id) >= (SELECT COUNT(DISTINCT I_Id) / 2 FROM Stock WHERE K_Name = k.K_Name);
 
-SELECT i.S_Company, COUNT(*) AS Num_Ingredients
-FROM Ingredient i
-GROUP BY i.S_Company
-ORDER BY Num_Ingredients DESC;
-
-
-SELECT DISTINCT C_FirstName, C_LastName
-FROM Customer
-         JOIN Order ON Customer.C_Id = Order.C_Id
-         JOIN Meal ON Order.M_Id = Meal.M_Id
-         JOIN Kitchen ON Meal.K_Name = Kitchen.K_Name
-WHERE Kitchen.K_Name = 'Italian Kitchen'
-  AND Order_Date BETWEEN '2022-01-01' AND '2022-03-31';
--- To use indexing, we can create an index on the K_Name and Order_Date columns in the Order table since they're being used in the join condition and the WHERE clause. These indexes will allow for faster lookup of rows in the Order table when performing the join operation and filtering by date range.
-CREATE INDEX order_k_name ON Order (K_Name);
-CREATE INDEX order_order_date ON Order (Order_Date);
-
-SELECT Ingredient.I_Name, SUM(Ingredient.I_Calories) AS total_calories
-FROM Ingredient
-         JOIN Recipe ON Ingredient.I_Id = Recipe.I_Id
-         JOIN Meal ON Recipe.M_Id = Meal.M_Id
-WHERE Meal.M_Name = 'Spaghetti Carbonara'
-GROUP BY Ingredient.I_Id;
--- To use indexing, we can create an index on the M_Id column in the Recipe table since it's being used in the join condition. This index will allow for faster lookup of rows in the Recipe table when performing the join operation.
-CREATE INDEX recipe_m_id ON Recipe (M_Id);
-
-SELECT *
-FROM Meal
-WHERE M_Price < 10;
-CREATE INDEX idx_Meal_Price ON Meal (M_Price);
-
-
-SELECT DISTINCT M_Name
-FROM Meal
-         JOIN Recipe ON Meal.M_Id = Recipe.M_Id
-         JOIN Ingredient ON Recipe.I_Id = Ingredient.I_Id
-WHERE Ingredient.I_Name = 'Pasta';
--- To use indexing, we can create an index on the I_Name column in the Ingredient table since it's being used in the join condition. This index will allow for faster lookup of rows in the Ingredient table when performing the join operation.
-CREATE INDEX ingredient_i_name ON Ingredient (I_Name);
-
-
--- Here are 2 indexes that will not improve the performance on any of the queries:
--- 1. The index on the M_Id column in the Meal table will not improve the performance of the query in the first question because the M_Id column is not being used in the join condition or the WHERE clause.
-CREATE INDEX meal_m_id ON Meal (M_Id);
--- 2. The index on the I_Id column in the Ingredient table will not improve the performance of the query in the second question because the I_Id column is not being used in the join condition or the WHERE clause.
-CREATE INDEX ingredient_i_id ON Ingredient (I_Id);
-
+--8
+SELECT k.K_Name, COUNT(o.M_Id) AS TotalMeals, SUM(m.M_Price) AS TotalRevenue
+FROM Kitchen k
+         LEFT JOIN Meal m ON k.K_Name = m.K_Name
+         LEFT JOIN ”ORDER” o ON m.M_Id = o.M_Id
+WHERE o.Order_Date >= TO_DATE('2022-01-01','YYYY-MM-DD') AND o.Order_Date < TO_DATE('2023-10-01','YYYY-MM-DD')
+GROUP BY k.K_Name
+ORDER BY TotalRevenue DESC;
